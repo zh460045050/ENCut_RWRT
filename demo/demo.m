76dae8abd987a3a7ef32760bcc3ebd9d33093bf8
zh460045050 <32474206+zh460045050@users.noreply.github.com>
2022-05-14 10:42:20 +0800

    Add files via upload

---
+++ demo/demo.m
@@ -0,0 +1,25 @@
+clear;
+clc;
+addpath(genpath('encut_encode'));
+addpath(genpath('experiment'));
+addpath(genpath('external'));
+fileName = '253036.jpg';
+fileDir = 'img/';
+img = imread('/Users/zhulei/study/资料整理/Upgrade/CVPR2021/final_material/codes/LNSN-upload/demo_imgs/107793.jpg'); %image
+k = 25; %number of partitions
+thr = 0.3; %Threhod of the UCM
+
+disp('This is the demo of the ENCut......')
+
+%%%%%%Cluster-based ENCut Segmentation%%%%%%
+clusterBased(img, k);
+
+%%%%%%Cluster-based f-ENCut(fast exploring method) Segmentation%%%%%%
+f_clusterBased(img, k);
+
+%%%%%%Bipart-based ENCut Segmentation%%%%%%
+bipartBased(img, k);
+
+%%%%%%Hierichical-based ENCut Segmentation(with gPB)%%%%%%
+hierBased(img, thr);
+
